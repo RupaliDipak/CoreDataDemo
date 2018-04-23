3a797476db7d2a599694ef9ce36dd417074cd9ad
user <user@users-MacBook-Pro-2.local>
2018-04-23 12:38:36 +0530

    print("My core data demo")

--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnClick(_ sender: Any)
+    {
+        print("This is git demo")
+        print("My core data demo")
+    }
+    
 }
 
